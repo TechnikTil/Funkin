c9b1ebfb35e8741fda8d685f5c2e75c13738d14e
TechnikTil <techniktil@tilnotdrip.org>
2026-01-14 09:55:57 -0700

    Add Constants.MENU_MUSIC
    Co-authored-by: CrusherNotDrip <crushernotdrip@tilnotdrip.org>
    Signed-off-by: TechnikTil <techniktil@tilnotdrip.org>

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -2255,7 +2255,7 @@ class FreeplayState extends MusicBeatSubState
       FlxTransitionableState.skipNextTransOut = true;
       if (Type.getClass(_parentState) == MainMenuState)
       {
-        FunkinSound.playMusic('freakyMenu',
+        FunkinSound.playMusic(Constants.MENU_MUSIC,
           {
             overrideExisting: true,
             restartTrack: false,
